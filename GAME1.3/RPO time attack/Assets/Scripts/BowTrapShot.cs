7eda01010a58996c958712e107d355288a05601e
blazv <blazvidovic420@gmail.com>
2018-11-03 02:20:52 +0100

    Game verzija 1.3

---
+++ GAME1.3/RPO time attack/Assets/Scripts/BowTrapShot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowTrapShot : MonoBehaviour {
+
+    private Trap_player bowscript;
+
+    public MoveBullet arrow;
+    public Transform firePoint;
+    public Transform firePoint2;
+
+    private bool bowreload = false;
+    // Use this for initialization
+    void Start () {
+
+        GameObject player = GameObject.FindWithTag("Player");
+        bowscript = player.GetComponent<Trap_player>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (bowscript.shootarrow == true && bowreload == false) 
+        {
+            Debug.Log("You stepped in bow trap.");
+            Instantiate(arrow, firePoint.position, firePoint.rotation);
+            Instantiate(arrow, firePoint2.position, firePoint2.rotation);
+
+            bowscript.shootarrow = false;
+            StartCoroutine(Waitbow());  //počaka da se lok znova napne
+        }
+        
+
+    }
+
+    IEnumerator Waitbow()
+    {
+        yield return new WaitForSeconds(1.6f);
+        bowscript.bowreload = false; //lok je znova napet
+    }
+}
---
+++ GAME1.3/RPO time attack/Assets/Scripts/Trap_player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_player : MonoBehaviour {
+
+    private PlayerMovement playerScript;
+    public Vector2 startPosition = Vector2.zero;
+    public bool shootarrow;// = false;
+    public bool bowreload = false;
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        startPosition = player.transform.position; //dobi zacetno pozicijo Playerja
+        Debug.Log(startPosition);
+
+        playerScript = player.GetComponent<PlayerMovement>();
+        shootarrow = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) //ce se sprozi trigger
+    {
+        if (other.CompareTag("KillTrap"))
+        {
+            Debug.Log("You got killed");
+            playerScript.transform.position = startPosition; //playerja vrze na zacetno pozicijo
+        }
+
+        if (other.CompareTag("SlowTrap"))
+        {
+            float temp = playerScript.speed;
+            Debug.Log("You are slowed down for 5s");
+            playerScript.speed = playerScript.speed * 0.3f; //playerja upočasni
+
+            StartCoroutine(Wait(temp)); //pocakaj 5s
+        }
+
+        if (other.CompareTag("BowTrigger") && shootarrow == false && bowreload == false)  //če player stopi v bowtriggerwire naredi ustreli puscico, in zacne reload loka
+        {
+            shootarrow = true;
+            bowreload = true;
+        }
+    }
+    
+
+    IEnumerator Wait(float temp)
+    { 
+        yield return new WaitForSeconds(5.0f);
+
+        playerScript.speed = temp;
+        Debug.Log("Znova si hiter");
+    }
+}
